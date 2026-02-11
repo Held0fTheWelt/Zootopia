c46d382a76694859c179d467d4e02ae23cc2e809
Yves Tanas <Yves.Tanas@gmx.de>
2026-02-11 16:47:53 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,18 +10,30 @@ def load_data(file_path):
 template_file = open("animals_template.html")
 
 animals_data = load_data('animals_data.json')
-output = ''  # define an empty string
+output = ""
 for animal in animals_data:
-    if 'name' in animal.keys():
-        output += f"Name: {animal['name']}\n"
-    if 'diet' in animal['characteristics'].keys():
-        output += f"Diet: {animal['characteristics']['diet']}\n"
-    if 'locations' in animal.keys():
-        output += f"Location: {animal['locations'][0]}\n"
-    if 'type' in animal['characteristics'].keys():
-        output += f"Diet: {animal['characteristics']['type']}\n"
+    output += '<li class="cards__item">\n'
+
+    if 'name' in animal:
+        output += f"Name: {animal['name']}<br/>\n"
+
+    if 'characteristics' in animal:
+        chars = animal['characteristics']
+        if 'diet' in chars:
+            output += f"Diet: {chars['diet']}<br/>\n"
+
+    if 'locations' in animal and animal['locations']:
+        output += f"Location: {animal['locations'][0]}<br/>\n"
+
+    if 'characteristics' in animal:
+        chars = animal['characteristics']
+        if 'type' in chars:
+            output += f"Type: {chars['type']}<br/>\n"
+
+    output += "</li>\n"
 
 text = template_file.read().replace("__REPLACE_ANIMALS_INFO__", output)
+
 
 with open("animals.html", "w") as output_file:
   output_file.write(text)
