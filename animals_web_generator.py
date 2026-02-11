7f7bc5ebdb50195e8933484db45b81b1ba7cef8d
Yves Tanas <Yves.Tanas@gmx.de>
2026-02-11 17:46:20 +0100

    added serialization function for animal object

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,31 +6,35 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
+def serialize_animal(animal_obj):
+    animal_output = ""
+    animal_output += '<li class="cards__item">\n'
+
+    if 'name' in animal:
+        animal_output += f'<div class="card__title">{animal['name']}</div>\n'
+        animal_output += '<p class="card__text">'
+        if 'characteristics' in animal:
+            chars = animal['characteristics']
+            if 'diet' in chars:
+                animal_output += f"<strong>Diet:</strong> {chars['diet']}<br/>\n"
+
+        if 'locations' in animal and animal['locations']:
+            animal_output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
+
+        if 'characteristics' in animal:
+            chars = animal['characteristics']
+            if 'type' in chars:
+                animal_output += f"<strong>Type:</strong> {chars['type']}<br/>\n"
+        animal_output += '</p>'
+    animal_output += "</li>\n"
+    return animal_output
 
 template_file = open("animals_template.html")
 
 animals_data = load_data('animals_data.json')
 output = ""
 for animal in animals_data:
-    output += '<li class="cards__item">\n'
-
-    if 'name' in animal:
-        output += f'<div class="card__title">{animal['name']}</div>\n'
-        output += '<p class="card__text">'
-        if 'characteristics' in animal:
-            chars = animal['characteristics']
-            if 'diet' in chars:
-                output += f"<strong>Diet:</strong> {chars['diet']}<br/>\n"
-
-        if 'locations' in animal and animal['locations']:
-            output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
-
-        if 'characteristics' in animal:
-            chars = animal['characteristics']
-            if 'type' in chars:
-                output += f"<strong>Type:</strong> {chars['type']}<br/>\n"
-        output += '</p>'
-    output += "</li>\n"
+    output += serialize_animal(animal)
 
 text = template_file.read().replace("__REPLACE_ANIMALS_INFO__", output)
 
